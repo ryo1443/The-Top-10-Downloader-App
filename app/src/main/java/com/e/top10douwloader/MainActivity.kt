c89bf2e8c84808cd1c4b2a570cf10f1993ce2ff2
ryo <ryo12271443@icloud.com>
2020-06-19 14:01:52 +0900

    非同期通信を行う内部クラスを導入

--- app/src/main/java/com/e/top10douwloader/MainActivity.kt
+++ app/src/main/java/com/e/top10douwloader/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.e.top10douwloader
 
+import android.nfc.Tag
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"onCreate called")
+
     }
+
+    private inner class DownloadData: AsyncTask<String, Void, String>() {
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            TODO("Not yet implemented")
+        }
+    }
+
 }
